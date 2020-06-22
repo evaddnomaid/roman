70bc2086f0a6ff5cea8824c451e3eb6db5b715fe
Gus <augustin.burchell@gmail.com>
2020-06-21 21:31:51 -0500

    Test PEP8-ified; added non-working null value init

--- roman.py
+++ roman.py
@@ -1,27 +1,33 @@
 class Roman:
-	"""Roman numeral package"""
+    """Roman numeral package"""
 
-	ordinals = {'i': 'i', 'v': 'ii', 'x': 'iii'}
-	values   = {'i': '', 'v': 'iiiii', 'x': 'vv'}
+    ordinals = {'i': 'i', 'v': 'ii', 'x': 'iii', 'l': 'iiii'}
+    values = {'i': '', 'v': 'iiiii', 'x': 'vv', 'l': 'xxxxx'}
 
-	def __init__(self, r):
-		"""Initialize a new roman numeral"""
-		self.r = r
-	
-	def roman_numeral(self):
-		return self.r
+    def __init__(self, r=None):
+        """Initialize a new roman numeral"""
+        if r:
+            self.r = r
+        else:
+            self.r = ''
 
-	def add(self, x):
-		result = Roman(self.r)
-		result.r = self.r + x.r
-		result.order()
-		return result.r
+    def roman_numeral(self):
+        return self.r
 
-	def order(self):
-		i = len('i')
-		while i < len(self.r) and len(self.ordinals[self.r[i - 1:i]]) >= len(self.ordinals[self.r[i:i +1]]):
-			i += len('i')
-		if i < len(self.r):
-			self.r = self.r[0:i-1] + self.r[i:i+1] + self.r[i-1:i] + self.r[i+1:]
-			self.order()
+    def add(self, x):
+        result = Roman(self.r)
+        result.r = self.r + x.r
+        result.order()
+        return result.r
+
+    def order(self):
+        """
+        Sorts input roman characters based on corresponding self.ordinals value
+        """
+        i = len('i')
+        while i < len(self.r) and len(self.ordinals[self.r[i - 1:i]]) >= len(self.ordinals[self.r[i:i +1]]):
+            i += len('i')
+        if i < len(self.r):
+            self.r = self.r[0:i-1] + self.r[i:i+1] + self.r[i-1:i] + self.r[i+1:]
+            self.order()
 
--- test_roman.py
+++ test_roman.py
@@ -2,26 +2,41 @@ import unittest
 from roman import Roman
 
 class TestRoman(unittest.TestCase):
-	"""Tests for 'Roman.py'."""
+    """Tests for 'Roman.py'."""
 
-	def test_adding_x_to_x(self):
-		a = Roman('x')
-		b = Roman('x')
+    def test_adding_x_to_x(self):
+        """ Test adding 10 + 10 """
+        numeral_a = Roman('x')
+        numeral_b = Roman('x')
 
-		self.assertEqual("xx", a.add(b))
-		self.assertEqual("xx", b.add(a))
+        self.assertEqual('xx', numeral_a.add(numeral_b))
+        self.assertEqual('xx', numeral_b.add(numeral_a))
 
-	def test_adding_vi_to_vi(self):
-		a = Roman('i')
-		b = Roman('vi')
-		self.assertEqual("vii", a.add(b))
-		self.assertEqual("vii", b.add(a))
+    def test_adding_i_to_vi(self):
+        """ Test adding 1 + 6 """
+        numeral_a = Roman('i')
+        numeral_b = Roman('vi')
+        self.assertEqual('vii', numeral_a.add(numeral_b))
+        self.assertEqual('vii', numeral_b.add(numeral_a))
 
-	def test_adding_xi_to_vi(self):
-		a = Roman('xi')
-		b = Roman('vi')
-		self.assertEqual("xvii", a.add(b))
-		self.assertEqual("xvii", b.add(a))
+    def test_adding_xi_to_vi(self):
+        """ Test adding 11 + 6 """
+        numeral_a = Roman('xi')
+        numeral_b = Roman('vi')
+        self.assertEqual('xvii', numeral_a.add(numeral_b))
+        self.assertEqual('xvii', numeral_b.add(numeral_a))
+
+    def test_adding_l_to_i(self):
+        """ Test adding 50 + 1 """
+        numeral_a = Roman('l')
+        numeral_b = Roman('i')
+        self.assertEqual('li', numeral_a.add(numeral_b))
+        self.assertEqual('li', numeral_b.add(numeral_a))
+
+    # def test_empty_init(self):
+    #     """ Test creating an 'empty' numeral (controversial!) """
+    #     numeral_a = Roman()
+    #     self.assertEqual('', numeral_a)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
